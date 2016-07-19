d26c099988dff5f480cc8ac21281e18f30499fb1
s8521 <a35316981@gmail.com>
2016-07-19 21:22:07 +0800

    相機功能

--- app/src/main/java/com/sakusai/application8429/Photo.java
+++ app/src/main/java/com/sakusai/application8429/Photo.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Photo extends AppCompatActivity {
@@ -30,6 +33,17 @@ public class Photo extends AppCompatActivity {
         startActivityForResult(intent, 1);
     }
 
+    public void cameraphoto(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //調用系統相機
+
+        Uri imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"image.jpg"));
+        //指定照片保存路徑（SD卡），image.jpg为一個臨時文件，每次拍照後這個圖片都會被替換
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+        //直接使用，沒有縮小
+        startActivityForResult(intent, 2);  //用戶點擊了從相機獲取
+    }
+
     public void toPhotocomparison(View v){
         Intent intent = new Intent();
         intent.setClass(Photo.this, Photocomparison.class);
@@ -39,7 +53,7 @@ public class Photo extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         //當使用者按下確定後
-        if (resultCode == RESULT_OK) {
+        if ((requestCode == 1 || requestCode == 2 ) && data != null) {
             //取得圖檔的路徑位置
             Uri uri = data.getData();
             //寫log
